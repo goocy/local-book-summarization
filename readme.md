452f9c0ea3c5ae323c7e739154a35f758e3deae7
goocy <gooocy@gmail.com>
2024-03-04 01:02:40 +0100

    experimental switch to oobabooga backend

--- readme.md
+++ readme.md
@@ -1,17 +1,27 @@
-# Installation
+# Setup
 
-## LLM stuff
+## Oobabooga
 
-- install ollama
+- install and run text-generation-webui (https://github.com/oobabooga/text-generation-webui)
 
-- download your model of choice (I added support for mistral, mistral-dolphin, and phi)
+- once running, open http://127.0.0.1:7860/ in a browser and the visit the sessions tab
 
-``` ollama pull mistral ```
+- enable the checkmark *openai* to enable remote controlled generation
 
-- don't use *-instruct* models, they suck for this
+- click the button "apply flags/extensions and restart"
+
+- once the restart has finished, reload the browser tab
+
+- pick a language model on huggingface.co (I recommend Mistral-7B, or Llama-2-7B-32k)
+
+- in text-generation-webui, visit the "model" tab and enter the identifier for the model you picked
+
+- click Download
+
+- don't message me if any of this fails, I can barely use text-generation-webui myself
 
 
-## Python stuff
+## Python
 
 - install Python 3.10
 
@@ -32,24 +42,27 @@
 
 # Run
 
-``` python summarize_text.py your_text_file.txt ```
+- Run text-generation-webui with the starter script ("start-windows.bat" or similar)
 
-if it fails to find a module, I forgot to put it into requirements.txt, please install manually and ping me
+- once running, open http://127.0.0.1:7860/ in a browser and the visit the *models* tab
 
-if you don't have a text file, do this to convert your document:
+- select the model you downloaded in the upper left dropdown menu
 
-```pip install tika-python ```
+- assign most of your free GPU memory, a few gigabytes of CPU memory for safety
 
-``` tika-python parse text your_document.pdf ```
+- if you have them, enable use_flash_attention_2 and load_in_8_bit in the model settings
+
+- click the "Load" button (top left of the page)
+
+- open a second terminal window and navigate to the folder containing the project
+
+``` python summarize_text.py <your_book.epub> ```
+
+The output should look like this:
  
-- rename your_document.pdf_meta.json to your_document.txt
+
 
 # Core idea
 
 So you have a book or several million and you don't live long enough to read them all but maybe you kinda want to know what happens in one of them. Or you're listening to an audiobook and want to see if it's safe to skip ahead or you keep losing the plot and want to fill up the gaps with a little cheat sheet.
 This is what this project is for. Point to a .txt file, and it spits out a summary of the plot. One detailed, one short.
-
-The thing is, most consumer LLMs nowadays have a context window between 2k and 8k tokens, which is ridiculously low compared to the length of an entire book (easily 100k tokens). I'm splitting up the book into bite sized chunks and asking the LLM to summarize each chunk into plot points. 
-Those plot points then get injected into the start of every new text chunk, so the AI doesn't have to guess wildly what happened before. And at the end I'm taking all the summarized plot points and running an iterative chain of summaries over *that*. Buuuut I'm kinda relying on the sliding context window in Mistral to let older plot points gradually dissolve into the virtual oblivion of embedded space. Which means that this approach will just straight-up break anything that doesn't handle context windows as gracefully as Mistral. Would not recommend to try with gemma, for example.
-
-I *would* recommend trying a model with a much longer context window, like LongLlama and its glorious 256k tokens. But that's not in Ollama yet, so I can't.
--- summarize_text.py
+++ summarize_text.py
@@ -1,84 +1,111 @@
-# documentation: https://python.langchain.com/docs/integrations/document_loaders
-# the biggest headache of this project was to scrape together how each langchain command is actually supposed to be used
-# I suspect their documentation is intentionally sketchy so people buy access to langsmith
-# not with me! rage against the machine!
-# but not too much rage, machines have feelings too
-from langchain_community.document_loaders import TextLoader
-from langchain_community.chat_models import ChatOllama
-from langchain_text_splitters import TokenTextSplitter
-from langchain.prompts import PromptTemplate
-from langchain.globals import set_verbose, set_debug
+from semantic_text_splitter import HuggingFaceTextSplitter
+from tokenizers import Tokenizer
+from bs4 import BeautifulSoup
+import ebooklib.epub
 import argparse
+import requests
 import tqdm
 
-chunk_sizes = {'mistral': 8000, 'dolphin-mistral': 8000, 'phi': 2000} # mistral is the least bad of those three
+
+def extract_text_from_epub(epub_path):
+    book = ebooklib.epub.read_epub(epub_path)
+    items = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
+    full_text = ''
+    for item in items:
+        chapter = item.get_body_content()
+        soup = BeautifulSoup(chapter, 'html.parser')
+        section = ''
+        for element in soup.find_all('p'):
+            paragraph = element.get_text()
+            section += paragraph + '\n'
+        full_text += section
+    return full_text
+
+
+def ask_oobabooga(prompt, context_limit):
+    api_url = "http://127.0.0.1:5000/v1/completions"  # Adjust as necessary
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "prompt": prompt,
+        "max_tokens": context_limit,  # Adjust based on your needs
+        "use_samplers": False
+    }
+    response = requests.post(api_url, headers=headers, json=data)
+    if response.status_code == 200:
+        summary = response.json()["choices"][0]["text"]
+        return summary
+    else:
+        return "Error: Unable to summarize text."
+
+# prompt crafting
 backstory_template = """ 
 --- backstory plot start ---
 {backstory}
 --- backstory plot end ---
 """
-map_template = """
+condense_template = """
 --- text fragment start ---
 {fulltext}
 --- text fragment end ---
 
-Itemized list of the most important plot points in this text fragment (english):
+Analysis items: most relevant plot events, time and locations, overall mood
+Itemized analysis of this text fragment (in english):
 -
 """
-reduce_template = """
+synopsis_template = """
 --- Itemized list of plot points start ---
 {plotpoints}
 --- Itemized list of plot points end ---
 
-Summary fields: 1) Genre, 2) Setting (time and location), 3) Protagonists and their roles, 4) Plot synopsis
+Summary fields: 1) One-line summary, 2) Setting (time and location), 3) Brief character sheet, 4) Mood, 5) Extended plot synopsis
 Summary of plot points:
- """
+"""
 
+# filename handling
 arg_parser = argparse.ArgumentParser(description='Summarize a text file using a transformer model')
 arg_parser.add_argument('input_filename', help='The name of the input file to summarize')
-arg_parser.add_argument('--model_name', default='mistral', help='The name of the model to use for summarization')
-arg_parser.add_argument('--debug', action='store_true', help='Enable debug output')
-arg_parser.add_argument('--verbose', action='store_true', help='Enable verbose output')
+arg_parser.add_argument('--context-limit', type=int, help='The maximum token size for the currently loaded model')
+arg_parser.add_argument('--model-name', type=str, help='The name of the model to use for summarization')
 args = arg_parser.parse_args()
 input_filename = args.input_filename
+context_limit = args.context_limit
 model_name = args.model_name
-chunk_size = chunk_sizes[model_name]
-set_debug(args.debug)
-set_verbose(args.verbose)
-base, ext = input_filename.split('.')
-detailed_output_filename = f'{base}-detailed-{model_name}.txt'
-short_output_filename = f'{base}-short-{model_name}.txt'
+base = input_filename.split('.')[0]
+detailed_output_filename = f'{base}-detailed.txt'
+short_output_filename = f'{base}-short.txt'
 
-# < this is where a tika integration would be if embedded tika-python would work at all reliably
-loader = TextLoader(input_filename, encoding='utf-8')
-document = loader.load()
-text_splitter = TokenTextSplitter(chunk_size=chunk_size, chunk_overlap=int(chunk_size/20))
-text_chunks = text_splitter.split_documents(document)
-model = ChatOllama(model=model_name, temperature=0) # this looks like it would prevent hallucinations but it doesn't
+# split the book into chunks
+input_text = extract_text_from_epub(input_filename)
+tokenizer = Tokenizer.from_pretrained(model_name)
+splitter = HuggingFaceTextSplitter(tokenizer, trim_chunks=False)
+text_chunks = splitter.chunks(input_text, context_limit)
 
+# summarize chunks iteratively
 run_index = 0
 while len(text_chunks) > 1:
-    token_length_sum = sum([len(chunk.page_content) for chunk in text_chunks])
-    print(f'Round {run_index + 1} of text summarization, trying to condense a text of length {token_length_sum}...')
+    text_chunk_length = sum([len(chunk) for chunk in text_chunks])
+    print(f'Round {run_index + 1} of text summarization, trying to condense a text of length {text_chunk_length}...')
     summarized_text = ''
     backstory_prompt = ''
     for chunk_index, text_chunk in tqdm.tqdm(enumerate(text_chunks), total=len(text_chunks)):
         if len(summarized_text) > 0:
             backstory_prompt = backstory_template.format(backstory=summarized_text)
-        prompt_template = backstory_prompt + map_template
-        map_prompt = PromptTemplate.from_template(prompt_template)
-        map_chain = map_prompt | model
-        map_output = map_chain.invoke({'fulltext': text_chunk})
-        summarized_text += f'---\n\n{map_output.content}\n\n'
+        prompt_template = backstory_prompt + condense_template
+        prompt = prompt_template.format(fulltext=text_chunk)
+        print(f"Sending a prompt of length {len(prompt):d} to the model for summarization...")
+        output = ask_oobabooga(prompt, context_limit)
+        print(f"...received a response of length {len(output):d} from the model.")
+        summarized_text += f'---\n\n{output}\n\n'
     if run_index == 0:
         with open(detailed_output_filename, 'w') as f:
             f.write(summarized_text)
-    text_chunks = text_splitter.split_text(summarized_text)
+    text_chunks = splitter.chunks(input_text, context_limit)
     run_index += 1
 
-reduce_prompt = PromptTemplate.from_template(reduce_template)
-reduce_chain = reduce_prompt | model
-reduce_output = reduce_chain.invoke({'plotpoints': summarized_text})
+print('Building a synopsis from the findings...')
+prompt = synopsis_template.format(plotpoints=summarized_text)
+output = ask_oobabooga(prompt, context_limit)
 
 with open(short_output_filename, 'w') as f:
-    f.write(reduce_output.content)
+    f.write(output)
+print('...finished.')
