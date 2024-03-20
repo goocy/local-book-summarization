45da2cf784536788406b7defec0d1690a527a3f8
goocy <gooocy@gmail.com>
2024-03-20 22:40:22 +0100

    Fixed a major bug with the strong summarization

--- summarize_text.py
+++ summarize_text.py
@@ -1,3 +1,5 @@
+import pickle
+
 import sentence_splitter
 from bs4 import BeautifulSoup
 import ebooklib.epub
@@ -22,25 +24,30 @@ class Summarizer:
             'top_k': 20,
             'top_p': 0.5
         }
-        self.prompt_template = """
+        self.summary_template = """
         --- Start of backstory ---
         {backstory}
         --- End of backstory ---
 
+        """
+        self.prompt_template = """
         --- Start of text section ---
         {full_text}
         --- End of text section ---
 
         Summary format: english, 3-5 sentences
         Summary of text section:
-
+        
         """
 
     def load(self, input_filepath):
-        filename = os.path.split(input_filepath)[-1]
-        base, ext = os.path.splitext(filename)
-        self.detailed_output_filename = f'{base}-detailed.txt'
-        self.short_output_filename = f'{base}-short.txt'
+        filepath_parts = os.path.split(input_filepath)
+        root_path = filepath_parts[0]
+        base, ext = os.path.splitext(filepath_parts[-1])
+        detailed_filename = f'{base}-detailed.txt'
+        short_filename = f'{base}-short.txt'
+        self.detailed_output_filepath = os.path.join(root_path, detailed_filename)
+        self.short_output_filepath = os.path.join(root_path, short_filename)
         if input_filepath.endswith('.epub'):
             return self.load_epub(input_filepath)
         elif input_filepath.endswith('.json'):
@@ -84,39 +91,55 @@ class Summarizer:
         tokens = self.tokenizer.encode(text)
         return len(tokens.ids)
 
-    def calculate_text_split(self, text, token_limit, from_the_end=False, contingency=True):
+    def split_text(self, text, token_limit, from_the_end=False, contingency=True):
         if len(text) == 0:
-            return '', None
+            return '', ''
 
-        # Check if the entire text is short enough to summarize it in one go
-        if self.measure_tokens(text) < token_limit:
-            return None
+        # Check if the entire text happens to be shorter than the token limit
+        text_length = self.measure_tokens(text)
+        if text_length < token_limit:
+            return text, ''
 
         # Avoid splitting the text in the middle of a sentence
         sentences = sentence_splitter.split(text)
         if from_the_end:
             sentences.reverse()
 
-        # Do a virtual run through all sentences to see how many we can fit into the token limit
-        chunk_length = 0
-        remainder_length = len(text)
-        combined_token_length = 0
-        for sentence_index, sentence in enumerate(sentences):
-            token_length = self.measure_tokens(sentence)
-            # If the next sentence would exceed the token limit, that means that the current sentence_index is the limit
-            if combined_token_length + token_length > token_limit:
-                break
-            # update the running tallies
-            combined_token_length += token_length
-            chunk_length += len(sentence)
-            remainder_length -= len(sentence)
-            # Contingency case: before the remainder gets too short, we'd rather not try to fill the token limit
-            if contingency and chunk_length > remainder_length:
-                break
+        # Contingency: if we're not more than twice over the token limit, we should split the text in half
+        if contingency:
+            if text_length < token_limit * 2:
+                token_limit = text_length // 2
 
-        # convert the sentence index into a character index
-        text_index = sum(len(sentence) for sentence in sentences[:sentence_index])
-        return text_index
+        # Run a binary search to find the split point that fits the maximum amount of text into the token limit
+        low = 0
+        high = len(sentences)
+        best_split_point = 0
+        while low < high:
+            mid = (low + high) // 2
+            first_part_test = ' '.join(sentences[:mid])
+            test_length = self.measure_tokens(first_part_test)
+            if test_length < token_limit:
+                best_split_point = mid  # this split is valid, might be the best one so far
+                low = mid + 1  # look for a split that allows more text in the first part
+            else:
+                high = mid  # look for a split that puts less text in the first part
+        sentence_split_point = best_split_point
+
+        if from_the_end:
+            sentences.reverse()
+            last_part = ' '.join(sentences[-sentence_split_point:])
+            if sentence_split_point == len(sentences):
+                remaining_part = ''
+            else:
+                remaining_part = ' '.join(sentences[:-sentence_split_point])
+            return last_part, remaining_part
+
+        first_part = ' '.join(sentences[:sentence_split_point])
+        if sentence_split_point == len(sentences):
+            remaining_part = ''
+        else:
+            remaining_part = ' '.join(sentences[sentence_split_point:])
+        return first_part, remaining_part
 
     def create_context(self, round_index, section_index):
         responses = self.responses[round_index]
@@ -129,62 +152,74 @@ class Summarizer:
             return context
 
         # weak backstory means that we forget the first part of the context once our context space is exceeded
-        if self.backstory_strength == 'weak':
-            split_index = self.calculate_text_split(context, context_token_limit, from_the_end=True, contingency=False)
-            discard_ratio = 1 - split_index / len(context)
-            print(f'Context limit exceeded, omitting the first {discard_ratio:.0%} of this round\'s context...')
-            return context[-split_index:]
+        if self.backstory_strength == 'weak' and section_index is not None:
+            text_chunk, remaining_chunk = self.split_text(context, context_token_limit, from_the_end=True, contingency=False)
+            discard_ratio = len(remaining_chunk) / len(context)
+            print(f'Context limit exceeded, omitting the first {discard_ratio:.0%} of context...')
+            return text_chunk
 
         # strong backstory means that we condense the previous condensed context even further
         input_context = context
         condensed_context = ''
-        print(f'\nCondensing previous context of round {round_index+1:d} before continuing...')
+        input_context_length = self.measure_tokens(input_context)
+        print(f'\nCondensing {input_context_length:d} tokens of context before continuing...')
         # this needs to be done every time a new text chunk comes in, so it takes more time overall
         while True:
             # process the text in chunks small enough to fit into the LLM's context window
-            condensate, context_split_index = self.condense_text(input_context, condensed_context)
+            condensate, remaining_context = self.condense_text(input_context, condensed_context)
+            condensed_context += condensate
 
             # check if we can wrap up this round because the entire input text has been condensed
-            if context_split_index is None:
+            if len(remaining_context) == 0:
                 break
 
             # not done? aw. prepare for the next round
-            condensed_context += condensate
-            input_context = input_context[context_split_index:]
+            input_context = remaining_context
 
         print('...finished.')
         return condensed_context
 
     def condense_text(self, input_text, condensed_text):
-        # measure the structural (invariant) parts of the prompt
-        placeholder_prompt = self.prompt_template.format(full_text='', backstory=condensed_text)
-        placeholder_token_length = self.measure_tokens(placeholder_prompt)
-        if placeholder_token_length > self.model_token_limit:
+        # check if the invariate parts of the prompt exceed our token limit
+        if len(condensed_text) > 0:
+            summary_prompt = self.summary_template.format(backstory=condensed_text)
+            summary_length = self.measure_tokens(summary_prompt)
+        else:
+            summary_prompt = ''
+            summary_length = 0
+        template_length = self.measure_tokens(self.prompt_template)
+        if summary_length + template_length > self.model_token_limit:
             raise ValueError('This text is too long to summarize, at least with the default network. Try using one with a longer context window!')
 
         # try to squeeze as much input text as close as possible into the prompt
-        token_limit = self.model_token_limit - placeholder_token_length
-        split_index = self.calculate_text_split(input_text, token_limit)
-        text_chunk = input_text[:split_index]
-        main_prompt = self.prompt_template.format(full_text=text_chunk, backstory=condensed_text)
-        prompt = condensed_text + main_prompt
+        chunk_limit = self.model_token_limit - (summary_length + template_length)
+        text_chunk, remaining_chunk = self.split_text(input_text, chunk_limit)
+        main_prompt = self.prompt_template.format(full_text=text_chunk)
+        prompt = summary_prompt + main_prompt
+        prompt_length = self.measure_tokens(prompt)
 
         # ask an LLM to condense the full text for us
+        text_chunk_length = self.measure_tokens(text_chunk)
+        overhead_ratio = (prompt_length - text_chunk_length) / self.model_token_limit
+        print(f'New prompt: {text_chunk_length:d} tokens of text plus {overhead_ratio:.0%} overhead')
+        print(f'Sending {prompt_length:d} tokens to the LLM...')
         output = ollama.generate(model=self.model_name, prompt=prompt, options=self.ollama_options)
 
         # gather the LLM output
         response = output['response']
+        response_length = self.measure_tokens(response)
         processing_time = output['total_duration'] / 1000000000
-        if split_index is not None:
-            processing_speed = split_index / processing_time
-            print(f'...processed {split_index:d} characters in {processing_time:.0f} seconds ({processing_speed:.0f} ch/s).')
-            print(f'Remaining text length: {len(input_text)-split_index:d} characters')
-        return response, split_index
+        if len(text_chunk) is not None:
+            remaining_text_length = self.measure_tokens(remaining_chunk)
+            processing_speed = (prompt_length + response_length) / processing_time
+            print(f'...finished in {processing_time:.0f} seconds ({processing_speed:.0f} t/s).')
+            print(f'LLM summary: {response_length:d} tokens')
+            print(f'Remaining input text: {remaining_text_length:d} tokens')
+        return response, remaining_chunk
 
     def summarize(self, input_text):
-        input_token_length = self.measure_tokens(input_text)
-        input_text_length = len(input_text)
-        print(f'Trying to condense {input_text_length:d} characters ({input_token_length:d} tokens)...')
+        input_text_length = self.measure_tokens(input_text)
+        print(f'Trying to condense a text with a length of {input_text_length:d} tokens...')
 
         run_index = 0
         self.responses = []
@@ -196,13 +231,13 @@ class Summarizer:
             while True:
                 # process the text in chunks small enough to fit into the LLM's context window
                 print(f'\nRound {run_index+1:d}-{section_index+1:d}...')
-                response, text_split_index = self.condense_text(input_text, condensed_text)
+                response, remaining_chunk = self.condense_text(input_text, condensed_text)
 
                 # store the results
                 self.responses[run_index].append(response)
 
                 # check if we can wrap up this round because the entire input text has been condensed
-                if text_split_index is None:
+                if len(remaining_chunk) == 0:
                     break
 
                 # not done? aw. prepare for the next round
@@ -211,23 +246,26 @@ class Summarizer:
                     condensed_text = self.create_context(run_index, section_index)
                 else:
                     condensed_text = ''
-                input_text = input_text[text_split_index:]
+                input_text = remaining_chunk
 
-            condensation_factor = 1 - len(condensed_text) / input_text_length
+            # collect all the condensed text from this round
+            summary_text = self.create_context(run_index, None)
+            condensation_factor = 1 - self.measure_tokens(summary_text) / input_text_length
             print(f'...finished round {run_index+1:d}! Condensed the text by {condensation_factor:.2%}.')
 
-            # check if our condensed responses are short enough to be considered a summary
-            condensed_text = self.create_context(run_index, None)
-            if len(condensed_text) < self.reasonable_text_length:
-                with open(self.short_output_filename, 'w', encoding='utf-8') as f:
-                    f.write(condensed_text)
-                print(f'Processing complete! Wrote a brief summary to {self.short_output_filename}.')
+            # write out the raw responses at the start of round 0
+            if run_index == 0:
+                with open(self.detailed_output_filepath, 'w', encoding='utf-8') as f:
+                    merged_responses = '\n\n'.join(self.responses[0])
+                    f.write(merged_responses)
+                print(f'Raw responses written to {self.detailed_output_filepath}.')
+
+            # if the summary has a reasonable length, we output it and wrap up
+            if len(summary_text) < self.reasonable_text_length:
+                with open(self.short_output_filepath, 'w', encoding='utf-8') as f:
+                    f.write(summary_text)
+                print(f'Processing complete!')
                 break
-            else:
-                if run_index == 0:
-                    with open(self.detailed_output_filename, 'w', encoding='utf-8') as f:
-                        f.write(condensed_text)
-                        print(f'Wrote a detailed summary to {self.detailed_output_filename}.')
 
             # nope not short enough. Try again, this time summarizing the condensed text
             run_index += 1
@@ -238,15 +276,28 @@ class Summarizer:
 
 def main():
     arg_parser = argparse.ArgumentParser(description='Summarize a large text file using a transformer model')
-    arg_parser.add_argument('input_filepath', help='The path to the input file to summarize')
+    arg_parser.add_argument('book_folderpath', help='The path to the folder that contains books')
     arg_parser.add_argument('--backstory_strength', default='strong', help='How strongly the the previous context should be tracked (none/weak/strong)')
     args = arg_parser.parse_args()
-    input_filepath = args.input_filepath
+    book_folderpath = args.book_folderpath
     backstory_strength = args.backstory_strength
+    valid_extensions = '.epub', '.txt', '.json'
 
     book_summarizer = Summarizer(backstory_strength)
-    input_text = book_summarizer.load(input_filepath)
-    condensed_text = book_summarizer.summarize(input_text)
+    for filename in os.listdir(book_folderpath):
+        base, ext = os.path.splitext(filename)
+        if ext not in valid_extensions:
+            continue
+        if ext == '.txt':
+            if base.endswith('-detailed') or base.endswith('-short'):
+                continue
+        input_filepath = os.path.join(book_folderpath, filename)
+        input_text = book_summarizer.load(input_filepath)
+        if os.path.exists(book_summarizer.detailed_output_filepath) or os.path.exists(book_summarizer.short_output_filepath):
+            print(f'Summary already exists for {filename}')
+            continue
+        print(f'Summarizing {filename}...')
+        condensed_text = book_summarizer.summarize(input_text)
 
 if __name__ == '__main__':
     main()
